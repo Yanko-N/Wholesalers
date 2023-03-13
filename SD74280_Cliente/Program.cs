ef530e09de30dbd69d7b0bd73d74cc05bfe9cd31
vitor novo <vitorjmnovo@gmail.com>
2023-03-13 17:42:39 +0000

    Desafio 1, soma de algoritmos da aula 06.03.2023

---
+++ SD74280_Cliente/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SD74280_Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient ClientSocket = new TcpClient("localhost", 1337); //criar um cliente
+
+            //escrever no ecra que o cliente foi conectado ao servidor
+            Console.WriteLine($"Connected to: {((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Address}:{((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Port}");
+
+            Thread thread = new Thread(() => {
+                Program.MainThread(ClientSocket);
+            });
+
+            thread.Start();
+
+            var message = Console.ReadLine();
+            while (!String.IsNullOrEmpty(message))
+            {
+                var bytesmessage = Encoding.UTF8.GetBytes(message);
+                ClientSocket.GetStream().Write(bytesmessage, 0, bytesmessage.Length);
+                message = Console.ReadLine();
+            }
+            ClientSocket.Client.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+
+        }
+
+        public static void MainThread(TcpClient ClientSocket)
+        {
+            try
+            {
+                while (ClientSocket.Connected)
+                {
+                    byte[] buffer = new byte[1024];
+                    int byte_count = ClientSocket.GetStream().Read(buffer, 0, buffer.Length);
+
+
+                    string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                    Console.WriteLine(data);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
