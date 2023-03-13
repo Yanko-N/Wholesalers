94cc1354a651feefa2f9695d47b86fab518a4fc7
Vasco Teixeira <al74107@utad.eu>
2023-03-13 16:27:00 +0000

    First Commit

---
+++ Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aula_2___Sockets___Client {
+    internal class Program {
+        static void Main(string[] args) {
+            TcpClient ClientSocket = new TcpClient("localhost", 1337);
+            Console.WriteLine($"Connected to: {((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Address}:{((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Port}");
+
+            Thread thread = new Thread(() => {
+                Program.MainThread(ClientSocket);
+            });
+            thread.Start();
+
+            var message = Console.ReadLine();
+            while (!String.IsNullOrEmpty(message)) {
+                var bytesmessage = Encoding.UTF8.GetBytes(message);
+                ClientSocket.GetStream().Write(bytesmessage, 0, bytesmessage.Length);
+                message = Console.ReadLine();
+            }
+            ClientSocket.Client.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+
+        }
+
+        public static void MainThread(TcpClient ClientSocket) {
+            try {
+                while (ClientSocket.Connected) {
+                    byte[] buffer = new byte[1024];
+                    int byte_count = ClientSocket.GetStream().Read(buffer, 0, buffer.Length);
+
+
+                    string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                    Console.WriteLine(data);
+                }
+            } catch (SocketException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
