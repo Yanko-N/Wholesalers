400fd60675ff7cc4e694ae8aa59e5c5654f6f5d6
vitor novo <vitorjmnovo@gmail.com>
2023-03-26 18:55:53 +0100

    The Client writes the ip, if any error the program writes the error and ends

--- TP1 - Cobertura em Wholesalers/Client/Program.cs
+++ TP1 - Cobertura em Wholesalers/Client/Program.cs
@@ -8,31 +8,80 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Aula_2___Sockets___Client {
-    internal class Program {
-        static void Main(string[] args) {
-            TcpClient ClientSocket = new TcpClient("localhost", 1337);
-            Console.WriteLine($"Connected to: {((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Address}:{((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Port}");
+namespace Aula_2___Sockets___Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
 
-            Thread thread = new Thread(() => {
-                Program.MainThread(ClientSocket);
-            });
-            thread.Start();
-            SendFile(ClientSocket, "./teste.csv");
-            var message = Console.ReadLine();
-            while (!String.IsNullOrEmpty(message)) {
-                var bytesmessage = Encoding.UTF8.GetBytes(message);
-                ClientSocket.GetStream().Write(bytesmessage, 0, bytesmessage.Length);
-                message = Console.ReadLine();
+            TcpClient ClientSocket = ConnectServer();
+
+            if (ClientSocket.Connected)
+            {
+                Console.WriteLine($"Connected to: {((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Address}:{((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Port}");
+
+
+                Thread thread = new Thread(() =>
+                {
+                    Program.MainThread(ClientSocket);
+                });
+                thread.Start();
+                //(ClientSocket, "./teste.csv");
+                var message = Console.ReadLine();
+                while (!String.IsNullOrEmpty(message))
+                {
+                    var bytesmessage = Encoding.UTF8.GetBytes(message);
+                    ClientSocket.GetStream().Write(bytesmessage, 0, bytesmessage.Length);
+                    message = Console.ReadLine();
+                }
+                ClientSocket.Client.Shutdown(SocketShutdown.Both);
+                ClientSocket.Close();
             }
-            ClientSocket.Client.Shutdown(SocketShutdown.Both);
             ClientSocket.Close();
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+
+
+
 
         }
 
-        public static void MainThread(TcpClient ClientSocket) {
-            try {
-                while (ClientSocket.Connected) {
+        public static TcpClient ConnectServer()
+        {
+            TcpClient ClientSocket = new TcpClient();
+
+            string[] ipBuffer;
+            while (true)
+            {
+                Console.WriteLine("Write the IP you want to connect to:");
+                ipBuffer = Console.ReadLine().Split(':');
+                if (ipBuffer.Length == 2)
+                {
+                    if (!String.IsNullOrEmpty(ipBuffer[0]) && !String.IsNullOrEmpty(ipBuffer[1]))
+                        break;
+                }
+            }
+
+            try
+            {
+                ClientSocket.Connect(ipBuffer[0], int.Parse(ipBuffer[1]));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}\n");
+            }
+
+            return ClientSocket;
+        }
+
+        public static void MainThread(TcpClient ClientSocket)
+        {
+            try
+            {
+                while (ClientSocket.Connected)
+                {
                     byte[] buffer = new byte[1024];
                     int byte_count = ClientSocket.GetStream().Read(buffer, 0, buffer.Length);
 
@@ -40,11 +89,15 @@ namespace Aula_2___Sockets___Client {
                     string data = Encoding.UTF8.GetString(buffer, 0, byte_count);
                     Console.WriteLine(data);
                 }
-            } catch (IOException e) {
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Failed to connect {e.Message}");
             }
         }
 
-        public static void SendFile(TcpClient ClientSocket, string path) {
+        public static void SendFile(TcpClient ClientSocket, string path)
+        {
             Console.WriteLine("Sending File");
             var buff = File.ReadAllBytes(path).Concat(Encoding.UTF8.GetBytes("\0\0\0")).ToArray();
             ClientSocket.GetStream().Write(buff, 0, buff.Length);
