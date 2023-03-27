f8abaccb69c8b5b9874d46a6cddbf7e77eaf300f
vitor novo <vitorjmnovo@gmail.com>
2023-03-27 19:28:13 +0100

    dataBase Created

---
+++ TP1 - Cobertura em Wholesalers/Server/Models/dataModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2___Sockets.Models
+{
+    public class dataModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Operador { get; set; }
+
+        [Required]
+        public string Municipio { get; set; }
+
+        [Required]
+        public string Rua { get; set; }
+
+        [Required]
+        public string Numero { get; set; }
+
+        [Required]
+        public string Apartamento { get; set; }
+
+        [Required]
+        public int Owner { get; set; }
+    }
+}
---
+++ TP1 - Cobertura em Wholesalers/Server/dataContext.cs
@@ -0,0 +1,21 @@
+﻿using Aula_2___Sockets.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2___Sockets
+{
+    public class dataContext : DbContext
+    {
+        public DbSet<dataModel> datas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=DataDB;Trusted_Connection=True;");
+        }
+        
+    }
+}
