6152b37b8d500970f62c0eed6fa790e996c308a8
Vasco Teixeira <al74107@utad.eu>
2023-04-14 22:03:38 +0100

    Operadora através do ficheiro de cobertura no commit anterior

--- TP1 - Cobertura em Wholesalers/Client/Program.cs
+++ TP1 - Cobertura em Wholesalers/Client/Program.cs
@@ -8,47 +8,26 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Aula_2___Sockets___Client
-{
-    internal class Program
-    {
-        public enum StatusCode
-        {
+namespace Aula_2___Sockets___Client {
+    internal class Program {
+        public enum StatusCode {
             OK = 100,
             ERROR = 300,
             BYE = 400
         }
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
 
             TcpClient ClientSocket = ConnectServer();
 
-            if (ClientSocket.Connected)
-            {
+            if (ClientSocket.Connected) {
                 Console.WriteLine($"Connected to: {((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Address}:{((IPEndPoint)ClientSocket.Client.RemoteEndPoint).Port}");
-
-                string operadora, certeza;
-                while (true)
-                {
-                    Console.WriteLine("What is your Operadora?");
-                    operadora = Console.ReadLine();
-                    Console.WriteLine("Are you sure?");
-                    certeza = Console.ReadLine();
-
-                    if (certeza.Contains("yes") || certeza.Contains("sim"))
-                        break;
-
-                }
-
-
 
                 byte[] buffer = new byte[1024];
                 string data;
 
                 data = GetDataFromStream(ClientSocket);
 
-                if (!data.Contains($"{(int)StatusCode.OK} - {StatusCode.OK}"))
-                {
+                if (!data.Contains($"{(int)StatusCode.OK} - {StatusCode.OK}")) {
                     Console.WriteLine($"Error: Expected '{(int)StatusCode.OK} - {StatusCode.OK}' \nClosing connection...");
                     ClientSocket.Client.Shutdown(SocketShutdown.Both);
                     ClientSocket.Close();
@@ -58,8 +37,7 @@ namespace Aula_2___Sockets___Client
 
 
                 string path;
-                do
-                {
+                do {
                     Console.Write("Full Path to File or Drag an Drop File: ");
                     path = Console.ReadLine();
                     path = path.Replace("\"", "");
@@ -86,12 +64,10 @@ namespace Aula_2___Sockets___Client
 
         }
 
-        public static string GetDataFromStream(TcpClient client)
-        {
+        public static string GetDataFromStream(TcpClient client) {
             byte[] buffer = new byte[1024];
             StringBuilder data = new StringBuilder();
-            do
-            {
+            do {
                 int byte_count = client.GetStream().Read(buffer, 0, buffer.Length);
                 data.Append(Encoding.UTF8.GetString(buffer, 0, byte_count));
             } while (!data.ToString().Contains("\0\0\0"));
@@ -99,28 +75,22 @@ namespace Aula_2___Sockets___Client
             return data.ToString();
         }
 
-        public static TcpClient ConnectServer()
-        {
+        public static TcpClient ConnectServer() {
             TcpClient ClientSocket = new TcpClient();
 
             string[] ipBuffer;
-            while (true)
-            {
+            while (true) {
                 Console.WriteLine("Write the IP you want to connect to:");
                 ipBuffer = Console.ReadLine().Split(':');
-                if (ipBuffer.Length == 2)
-                {
+                if (ipBuffer.Length == 2) {
                     if (!String.IsNullOrEmpty(ipBuffer[0]) && !String.IsNullOrEmpty(ipBuffer[1]))
                         break;
                 }
             }
 
-            try
-            {
+            try {
                 ClientSocket.Connect(ipBuffer[0], int.Parse(ipBuffer[1]));
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine($"{e.Message}\n");
                 Console.ReadKey();
             }
@@ -130,17 +100,13 @@ namespace Aula_2___Sockets___Client
         }
 
 
-        public static bool SendFile(TcpClient ClientSocket, string path)
-        {
-            try
-            {
+        public static bool SendFile(TcpClient ClientSocket, string path) {
+            try {
                 Console.WriteLine($"Sending File: {path}");
                 var buff = Encoding.UTF8.GetBytes(File.ReadAllText(path, Encoding.GetEncoding(1252)) + "\0\0\0");
                 ClientSocket.GetStream().Write(buff, 0, buff.Length);
                 return true;
-            }
-            catch (FileNotFoundException e)
-            {
+            } catch (FileNotFoundException e) {
                 Console.WriteLine(e.Message);
                 return false;
             }
