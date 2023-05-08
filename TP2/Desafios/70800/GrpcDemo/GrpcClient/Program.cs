f8c7199a1eb9f166d3b3b2c2ceb4c040a519ffec
gusta <gusta@DESKTOP-PGQ4MGH>
2023-05-08 17:42:35 +0100

    desafio

--- TP2/Desafios/70800/GrpcDemo/GrpcClient/Program.cs
+++ TP2/Desafios/70800/GrpcDemo/GrpcClient/Program.cs
@@ -11,12 +11,12 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var input = new HelloRequest { Name = "Santola" };
-            var channel = GrpcChannel.ForAddress("https://localhost:7275");
-            var client = new Greeter.GreeterClient(channel);
+            //var input = new HelloRequest { Name = "Santola" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:7275");
+            //var client = new Greeter.GreeterClient(channel);
 
-            var reply = await client.SayHelloAsync(input);
-            Console.WriteLine(reply.Message);
+            //var reply = await client.SayHelloAsync(input);
+            //Console.WriteLine(reply.Message);
 
             Console.ReadLine();
         }
