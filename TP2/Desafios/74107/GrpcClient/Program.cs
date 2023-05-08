1af80a50ffd1d6aae517fa2c53a2ebf4507c9bf5
Vasco Teixeira <al74107@utad.eu>
2023-05-08 17:19:23 +0100

    teste

--- TP2/Desafios/74107/GrpcClient/Program.cs
+++ TP2/Desafios/74107/GrpcClient/Program.cs
@@ -2,13 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GrpcClient {
     internal class Program {
         static async Task Main(string[] args) {
-            var channel = GrpcChannel.ForAddress("https://localhost:7264");
+            var channel = GrpcChannel.ForAddress("https://localhost:7264"); 
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                 new HelloRequest { Name = "GreeterClient" });
