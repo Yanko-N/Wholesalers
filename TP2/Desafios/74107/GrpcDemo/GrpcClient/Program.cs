6fa0bbe225bd379b0ae4e14864aff28262bdfcb5
Vasco Teixeira <al74107@utad.eu>
2023-05-09 18:17:25 +0100

    Desafio

--- TP2/Desafios/74107/GrpcDemo/GrpcClient/Program.cs
+++ TP2/Desafios/74107/GrpcDemo/GrpcClient/Program.cs
@@ -11,12 +11,22 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var input = new HelloRequest { Name = "Santola" };
+            var input = new In { In_ = 10};
             var channel = GrpcChannel.ForAddress("https://localhost:7275");
-            var client = new Greeter.GreeterClient(channel);
+            var client = new Converter.ConverterClient(channel);
 
-            var reply = await client.SayHelloAsync(input);
-            Console.WriteLine(reply.Message);
+            var reply = await client.C2FAsync(input);
+            Console.WriteLine(reply.Msg);
+            reply = await client.F2CAsync(input);
+            Console.WriteLine(reply.Msg);
+            reply = await client.D2EAsync(input);
+            Console.WriteLine(reply.Msg);
+            reply = await client.E2DAsync(input);
+            Console.WriteLine(reply.Msg);
+            reply = await client.K2MAsync(input);
+            Console.WriteLine(reply.Msg);
+            reply = await client.M2KAsync(input);
+            Console.WriteLine(reply.Msg);
 
             Console.ReadLine();
         }
