e99afee957a654c053310015830f3e57a2fc159c
Ricardo Silva <al70672@utad.eu>
2023-05-28 16:28:49 +0100

    Refactor Menu

--- TP2/Projeto/GrpcClient/Program.cs
+++ TP2/Projeto/GrpcClient/Program.cs
@@ -6,44 +6,82 @@ using System;
 using System.Text;
 using System.Threading.Channels;
 
-namespace GrpcClient
-{
-    class Program
-    {
+namespace GrpcClient {
+    class Program {
         static async Task Main(string[] args) {
+            var channel = GrpcChannel.ForAddress("https://localhost:7275");
             var user = new AuthUser();
             string authToken = "";
             bool isAdmin;
 
-           /* 
-            //RABBIT MQ CONFIGURATIONS
+            /* 
+             //RABBIT MQ CONFIGURATIONS
 
-            // Configuração da conexão com o RabbitMQ
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using var connection = factory.CreateConnection();
-
-
-            using var channelRabbit = connection.CreateModel();
-
-            // Declaração da exchange do tipo "topic"
-            channelRabbit.ExchangeDeclare("EVENT", ExchangeType.Topic);
+             // Configuração da conexão com o RabbitMQ
+             var factory = new ConnectionFactory() { HostName = "localhost" };
+             using var connection = factory.CreateConnection();
 
 
-            ConnectQueue(channelRabbit, "");
+             using var channelRabbit = connection.CreateModel();
 
-            */
+             // Declaração da exchange do tipo "topic"
+             channelRabbit.ExchangeDeclare("EVENT", ExchangeType.Topic);
+
+
+             ConnectQueue(channelRabbit, "");
+
+             */
+
+
+            LoginMenu(channel).ContinueWith((task) => {
+                user = task.Result.user;
+                isAdmin = task.Result.isAdmin ?? false;
+                authToken = task.Result.authToken ?? "";
+            }).Wait();
+
+            if (authToken == "") {
+                Console.WriteLine("Exiting the program...");
+                Console.ReadLine();
+                return;
+            }
+
 
 
             //Protos e Grpc Channel
-            var channel = GrpcChannel.ForAddress("https://localhost:7275");
-            var authClient = new Auth.AuthClient(channel);
+
             var adminClient = new AdminActions.AdminActionsClient(channel);
             var operatorClient = new OperatorActions.OperatorActionsClient(channel);
 
             //Main Menu System
+
+
+
+
+            using (var call = adminClient.ListAllCoberturas(new AdminActionsListAllCoberturas {
+                Operator = user.Username,
+                Token = authToken
+            })) {
+                while (await call.ResponseStream.MoveNext(CancellationToken.None)) {
+                    var curr = call.ResponseStream.Current;
+
+                    Console.WriteLine($"{curr.Operator} - {curr.Municipio} {curr.Rua} {curr.Numero} {curr.Apartamento ?? ""} {curr.Estado}");
+
+                }
+
+            }
+
+
+            Console.ReadLine();
+        }
+
+
+        static async Task<(AuthUser user, bool? isAdmin, string? authToken)> LoginMenu(GrpcChannel channel) {
+            var user = new AuthUser();
+            var authClient = new Auth.AuthClient(channel);
+            string? authToken = null;
+            bool? isAdmin = null;
             bool exit = false;
-            while (!exit)
-            {
+            while (!exit) {
                 Console.Clear();
                 Console.WriteLine("Welcome");
                 Console.WriteLine("Please select an option:");
@@ -54,11 +92,10 @@ namespace GrpcClient
 
                 string choice = Console.ReadLine();
                 //GRPC de Login
-                
+
                 AuthReply reply = new AuthReply();
 
-                switch (choice)
-                {
+                switch (choice) {
                     case "1":   //LOGIN
                         Console.Clear();
 
@@ -79,10 +116,9 @@ namespace GrpcClient
 
                         break;
 
-                    case "2":   //REGISTRO
+                    case "2":   //REGISTO
                         string confirm;
-                        do
-                        {
+                        do {
                             Console.Clear();
 
                             Console.Write("Username: ");
@@ -94,7 +130,7 @@ namespace GrpcClient
 
                             if (user.Password != confirm) //se as passwords dão match
                                 Console.WriteLine("Passwords do not match!");
-                                Console.ReadLine();
+                            Console.ReadLine();
                         } while (user.Password != confirm);
 
                         reply = await authClient.RegisterAsync(user);
@@ -105,7 +141,9 @@ namespace GrpcClient
                     case "3":
                         exit = true;
                         Console.WriteLine("Exiting the program...");
-                        return;
+                        isAdmin = null;
+                        authToken = null;
+                        break;
                     default:
                         Console.WriteLine("Invalid choice. Please try again.");
                         Console.ReadLine();
@@ -114,37 +152,17 @@ namespace GrpcClient
             }
 
 
-
-
-            using (var call = adminClient.ListAllCoberturas(new AdminActionsListAllCoberturas {
-                       Operator = user.Username,
-                       Token = authToken
-                   }))
-            {
-                while (await call.ResponseStream.MoveNext(CancellationToken.None))
-                {
-                    var curr = call.ResponseStream.Current;
-
-                    Console.WriteLine($"{curr.Operator} - {curr.Municipio} {curr.Rua} {curr.Numero} {curr.Apartamento ?? ""} {curr.Estado}");
-
-                }
-
-            }
-
-
-            Console.ReadLine();
+            return (user, isAdmin, authToken);
         }
 
-        static void ConnectRabitMQ(IModel channelRabbit,string topic)
-        {
+        static void ConnectRabitMQ(IModel channelRabbit, string topic) {
             // Criação de uma fila exclusiva e vinculação à exchange com uma chave de roteamento específica
             var queueName = channelRabbit.QueueDeclare().QueueName;
             channelRabbit.QueueBind(queueName, topic, "");
 
             // Configuração do consumidor para receber as mensagens
             var consumer = new EventingBasicConsumer(channelRabbit);
-            consumer.Received += (model, ea) =>
-            {
+            consumer.Received += (model, ea) => {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine("\nMensagem recebida: {0}", message);
